891aeb8ca423183a2479fedf2ad0963c33473b9b
viktorvidual <viktor.petrov@vidual.io>
2024-10-04 15:19:20 +0300

    Introducint Token Class + Basic Scanner

--- lox/Lox.java
+++ lox/Lox.java
@@ -1,4 +1,4 @@
-package com.craftinginterpreters.lox;
+package lox;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,14 +9,73 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
-  public static void main(String[] args) throws IOException {
-    if (args.length > 1) {
-      System.out.println("Usage: jlox [script]");
-      System.exit(64); 
-    } else if (args.length == 1) {
-      runFile(args[0]);
-    } else {
-      runPrompt();
+    static boolean hadError = false;
+    static boolean hadRuntimeError = false;
+
+
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            // the function works with only one argument. If there are more than one
+            // argument, it will print an error message and exit.
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            // if there is one argument, it will run the file
+            runFile(args[0]);
+        } else {
+            // if there is no argument, it will run the prompt
+            runPrompt();
+        }
     }
-  }
+
+    public static void runFile(String path) throws IOException {
+        // read the file from the path provided
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        // run the file
+        run(new String(bytes, Charset.defaultCharset()));
+
+        // Indicate an error in the exit code.
+        if (hadError)
+            System.exit(65);
+        if (hadRuntimeError)
+            System.exit(70);
+    }
+
+    public static void runPrompt() throws IOException {
+        // create a buffer reader to read the input from the user
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        // run the prompt for
+        for (;;) {
+            System.out.print("> ");
+            String line = reader.readLine();
+
+            if (line == null) {
+                break;
+            }
+            run(line);
+            hadError = false;
+        }
+    }
+
+    static void run(String source) {
+        // create instance of Scanner (which is a class still not implemented)
+        Scanner scanner = new Scanner(source);
+        // Create a list of tokens using the scanner
+        List<Token> tokens = scanner.scanTokens();
+
+        // print out all tokens
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    static void error(int line, String message) {
+        report(line, "", "message");
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println("[line] " + line + "] Error" + where + ": " + message);
+    }
 }
