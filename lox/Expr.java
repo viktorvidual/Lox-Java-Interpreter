c7259517d98ad403112cf389ee34e36c5c32520d
viktorvidual <viktor.petrov@vidual.io>
2024-10-08 10:17:45 +0300

    Adding Ast Printer for expression representation

--- lox/Expr.java
+++ lox/Expr.java
@@ -3,11 +3,24 @@ package lox;
 import java.util.List;
 
 abstract class Expr {
+  interface Visitor<R> {
+    R visitBinaryExpr(Binary expr);
+    R visitGroupingExpr(Grouping expr);
+    R visitLiteralExpr(Literal expr);
+    R visitUnaryExpr(Unary expr);
+  }
+
+  abstract <R> R accept(Visitor<R> visitor);
   static class Binary extends Expr {
     Binary(Expr left, Token operator, Expr right) {
       this.left = left;
       this.operator = operator;
       this.right = right;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitBinaryExpr(this);
     }
 
     final Expr left;
@@ -19,11 +32,21 @@ abstract class Expr {
       this.expression = expression;
     }
 
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitGroupingExpr(this);
+    }
+
     final Expr expression;
   }
   static class Literal extends Expr {
     Literal(Object value) {
       this.value = value;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitLiteralExpr(this);
     }
 
     final Object value;
@@ -32,6 +55,11 @@ abstract class Expr {
     Unary(Token operator, Expr right) {
       this.operator = operator;
       this.right = right;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitUnaryExpr(this);
     }
 
     final Token operator;
